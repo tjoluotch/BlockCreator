521ad050caa8e7d9d087b672258443ffce30bfc5
tjoluotch <trevorjoluotch@gmail.com>
2022-04-20 15:25:47 +0100

    changed main method implmentation

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"BlockCreator/internal/pkg/blockresolver/client"
+	blockresolver "BlockCreator/internal/pkg/blockresolver/configs/grpc"
 	"BlockCreator/internal/pkg/blockticker"
 	"fmt"
+	"log"
 )
 
 func main() {
@@ -15,10 +18,41 @@ func main() {
 	// the first block is created and prints in the console
 	fmt.Println("Initiate block creation...")
 
+	// grpc client initated to start streaming through block objects
+	conn, err := client.Start()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	log.Println("grpc connection created")
+	log.Printf("----------[%s]-----------\n", conn.GetState().String())
+
+	grpcClient := blockresolver.NewBlockResolverClient(conn)
+
+	// this should be a separate go routine if chan closes
+	// underneath block until chan closes see sendnblocks impl
+	pushChannel := make(chan struct{})
+	go func() {
+		for {
+			// start the process of sending a block once signal has been received from empty struct channel
+			//<-blockticker.SignalChan
+			if len(blockticker.BlockChan) < 0 {
+				continue
+			}
+			if err = client.PushBlock(grpcClient, client.ProcessBlock()); err != nil {
+				//	close channel
+				close(pushChannel)
+				log.Printf("Failed to send a block successfully: %v\n", err)
+				return
+			}
+		}
+	}()
+	<-pushChannel
+	log.Println("test")
 	// here we use an empty select{} in order to keep
 	// our main function alive indefinitely as it would
 	// complete before our BlockTicker has a chance
 	// to execute if we didn't.
-	// TODO possibly refactor to use wait group
+	// TODO refactor to use wait group
 	select {}
 }
